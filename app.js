2008a94735cc515c4584ceb183819889e29e3d87
t3oz <78038482+t3oz@users.noreply.github.com>
2023-02-01 00:00:59 +0100

    Update the port to listen

--- app.js
+++ app.js
@@ -168,8 +168,8 @@ app.get("/about", function(req, res) {
 });
 
 
+const port = process.env.PORT || 3000;
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started...");
 });
